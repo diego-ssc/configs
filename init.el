6d7ce45b0afb632f9be22d55449072b04a62c9b5
diego-ssc <diego_ssc@ciencias.unam.mx>
2023-03-07 07:05:21 -0600

    company-mode

--- init.el
+++ init.el
@@ -10,6 +10,12 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#ffffff" "#ffffff" "#ffffff" "#ffffff" "#928a7e" "#ffffff" "#ffffff" "gray90"])
+ '(company-backends
+   '(company-c-headers
+     (company-auctex-macros company-auctex-symbols company-auctex-environments company-keywords)
+     company-auctex-bibs company-auctex-labels company-bbdb company-semantic company-cmake company-capf company-clang company-files
+     (company-dabbrev-code company-gtags company-etags company-clang)
+     company-oddmuse company-dabbrev))
  '(custom-enabled-themes '(Lambda))
  '(custom-safe-themes
    '("652b55e5a130aa07f79c4c2d2101ff0fd6a3e189cdcd32654dc001a5070e533d" "7a4cc5d3766edb4ae68fe1decdbdac28a6891f2214659753d6c1cf6a953652a4" "bd4d3779a43199eecbf1caeb00c9de1d14b0a11d9eaf415e80f1e5a8a872c935" "fbef36c483d440ef0454ba905f0c686c3c802ebacf3e4050f9163809556cec19" "c453c41cabd16e5601297e29a43aa42db6571e9e6e5264d44a4ae10a80a9f85d" "37f54916e3e4cb0705176041c8a059b720e743040bcf77e5584ee79e90d529bd" "c52372f8865c19ac71477c35b06428f4b0fa989f092c0f64636d76d8096a1f1d" "4b2e7018ab458905983882edd73c89dbf4bf76d2e229e66bdcaf2ed02795ed54" "03aa756e02e64b21c0f53bd525c4901d52baec1f8f5a2bc332748b6943a7875b" "f76ebf242bba5e36f804980fdf07ee95306fa7c75fb3d4f63eb150c274a3dd7a" "b7bbd9fddd6796033d40ca9c4c1d8bfcc18b5963e08443cd60383d048245e24a" "bf083d5c07cb9301a7e798a6bd2e224e55cb40abb6006dd5eb47f9ba94548bcb" "715c5ce3485b5e442b413cfdaaba4ab22b9836983d9856541284ea0420ff73b4" "01254ddc7012bfc7bce3cc64e82f07c7f28fb827581a6aa98f497a30c3ea1a36" "4068a6bb6b5b143af76c843f1f72b3219d8f53691d9583c38c81943fcd807e78" "c4c83a3d29a78c7f30380c150658ce7eee91ddef6159cbbb8f6323585b4fd9f4" "11d4aef12dd4b9c008e005510a4cc35b3e797010d6ed7906cb6a3eaf4041590c" "0882034fa346f7288c51c93bb18664f30f4ca567e15fa67ea5c99099cde03711" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" "d0fd069415ef23ccc21ccb0e54d93bdbb996a6cce48ffce7f810826bb243502c" "ffba0482d3548c9494e84c1324d527f73ea4e43fff8dfd0e48faa8fc6d5c2bc7" "8f5b54bf6a36fe1c138219960dd324aad8ab1f62f543bed73ef5ad60956e36ae" "a36614356924f3cffc450c43d3ed8dace20218b41a1201f9c1173a1a0e0ac297" "3d3d7383a479aed14076ab057cc4bd6018cb6962ce4b197971d379eb99d0f420" "c832527516f15808747ca5c871ecba3a9f69d9b2a4ca8c63bcbb54ffbee462d1" "fa1ad9f82da8bed6eafa30379bef8e3c7531c76a16f3873a786ebafbca1474ca" "6ed2e9c641e0a1a440338fc81c2ac0fd52f8f434ff249dbd079c3e678fd7b1cc" "5d9639adbc5d2b31e3491ad378e342df9a69631c5d8fa6bd0802f6591aeddfdd" "aea4df979f530691c245ce8a597dde7f6fdb673971ea4a8d6f68024829e46bfb" "af3c418e2d74e769cfb6363be69afdeb8d22839f48fb5283036703b2a7e31f3c" "bab2326f7c5e8e9b6fd5692b0ef21a5bb9f650f8010ab316e0c642af4e0909f2" "41fbaada5d9f7605d393c49a2f75f05be2d10374600f5a8e31247e61b811377f" "5024e7c605ffbc687a5e2d0abcaddcad30d518a54b6974bb5ae616d6a83918a1" "75d8e70f2aa9768177c3595b003acb8fa5ee806b5e78e401f790ceb0ca0aaf8d" "89908a100287391ff0a5bc733f9f31fba7bc833094cbe92a8bfc9d9dd378a7e2" default))
@@ -30,6 +36,7 @@
  '(TeX-error-description-tex-said ((t (:foreground "#928a7e"))))
  '(alert-urgent-face ((t (:foreground "#ffffff" :weight bold))))
  '(ansi-color-blue ((t (:background "#ffffff" :foreground "#ffffff"))))
+ '(company-template-field ((t (:background "#222222" :foreground "#ffffff"))))
  '(compilation-error ((t (:foreground "#ffffff" :weight bold))))
  '(compilation-info ((t (:foreground "#ffffff" :weight bold))))
  '(compilation-mode-line-exit ((t (:inherit compilation-info :foreground "#ffffff" :weight bold))))
@@ -113,7 +120,7 @@
 ;;))
 ;;(add-hook 'server-after-make-frame-hook 'neotree-icons)
 
-;; Delete text that is being selected 
+;; Delete text that is being selected
 (delete-selection-mode 1)
 
 ;; PDF-opener : Zathura
@@ -141,12 +148,12 @@
 
 ;; Kill whole-line, copying it to clipboard
 ;; Sets `C-c d` to `M-x kill-whole-line`
-(global-set-key (kbd "C-c d") 'kill-whole-line)     
+(global-set-key (kbd "C-c d") 'kill-whole-line)
 
 ;; Every time when the neotree window is opened, let it find current file and jump to node.
 (setq neo-smart-open t)
 
-;; Sets the neotree buffer wodth to be variable. 
+;; Sets the neotree buffer wodth to be variable.
 (setq neo-window-fixed-size nil)
 
 ;; Disables line numbers for neotree
@@ -156,7 +163,7 @@
 
 ;; Sets the theme for neotree
 (setq neo-theme 'icons)
-;; (defun set-neotree-theme (_unused) 
+;; (defun set-neotree-theme (_unused)
 ;;   (interactive)
 ;; (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 ;; )
@@ -289,9 +296,10 @@
 (require 'company-auctex)
 (company-auctex-init)
 (add-hook 'after-init-hook 'global-company-mode)
-
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 2)
+(define-key company-active-map (kbd "<tab>") #'company-complete-selection)
 (add-to-list 'company-backends 'company-c-headers)
-
 ;; Auto-compete Auctex
 ;; (require 'auto-complete-auctex)
 
